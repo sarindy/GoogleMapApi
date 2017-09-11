a01a7376fd501c40865da1744f02783e561b289b
sarindy <sarindyo@yahoo.com>
2017-09-11 13:34:23 +0700

    Init

---
+++ src/main/java/com/example/GoogleMapApiApplication.java
@@ -0,0 +1,12 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoogleMapApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoogleMapApiApplication.class, args);
+	}
+}
