8c34f3cf7e4b703722ac23ab5441177a86df62c4
Sarindy Ouk <sarindyo@yahoo.com>
2017-09-26 13:20:41 +0700

    1414

--- src/main/resources/static/js/app.js
+++ src/main/resources/static/js/app.js
@@ -1,8 +1,9 @@
   function initMap(){
       // Map options
       var options = {
-        zoom:14,
-        center:{lat:11.553407,lng:104.928716}
+        zoom:18,
+        center:{lat:11.553407,lng:104.928716},
+        mapTypeId:'terrain'
       }
 
       // New map
@@ -13,23 +14,44 @@
         // Add marker
         addMarker({coords:event.latLng});
       });
+      
+      var contentString = '<div id="content">'+
+      '<div id="siteNotice">'+
+      '</div>'+
+      '<h1 id="firstHeading" class="firstHeading">Uluru</h1>'+
+      '<div id="bodyContent">'+
+      '<p><b>Uluru</b>, also referred to as <b>Ayers Rock</b>, is a large ' +
+      'sandstone rock formation in the southern part of the '+
+      'Northern Territory, central Australia. It lies 335&#160;km (208&#160;mi) '+
+      'south west of the nearest large town, Alice Springs; 450&#160;km '+
+      '(280&#160;mi) by road. Kata Tjuta and Uluru are the two major '+
+      'features of the Uluru - Kata Tjuta National Park. Uluru is '+
+      'sacred to the Pitjantjatjara and Yankunytjatjara, the '+
+      'Aboriginal people of the area. It has many springs, waterholes, '+
+      'rock caves and ancient paintings. Uluru is listed as a World '+
+      'Heritage Site.</p>'+
+      '<p>Attribution: Uluru, <a href="https://en.wikipedia.org/w/index.php?title=Uluru&oldid=297882194">'+
+      'https://en.wikipedia.org/w/index.php?title=Uluru</a> '+
+      '(last visited June 22, 2009).</p>'+
+      '</div>'+
+      '</div>';
 
       // Array of markers
       var markers = [
         {
           coords:{lat:11.553407,lng:104.928716},
           iconImage:'https://developers.google.com/maps/documentation/javascript/examples/full/images/beachflag.png',
-          content:'<h1>Lynn MA</h1>'
+          content:contentString
         },
         {
           coords:{lat:11.555401,lng:104.943450},
           iconImage:'https://developers.google.com/maps/documentation/javascript/examples/full/images/beachflag.png',
-          content:'<h1>Amesbury MA</h1>'
+          content:contentString
         },
         {
           coords:{lat:11.557703,lng:104.936562},
           iconImage:'https://developers.google.com/maps/documentation/javascript/examples/full/images/beachflag.png',
-          content:'<h1>Amesbury MA</h1>'
+          content:contentString
         }
       ];
 
